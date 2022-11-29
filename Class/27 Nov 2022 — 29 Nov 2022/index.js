db6d87eb6fa7d2800fe539dcb546a8ac7bd84021
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-11-29 09:17:24 +0530

    Variables in JavaScript

---
+++ Class/27 Nov 2022 — 29 Nov 2022/index.js
@@ -0,0 +1,33 @@
+// console.log("Hemlo")
+// console.log("This is a dummy log.")
+
+// const a = 10
+// let b = "20"
+// var c = 3.45
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+const a = 10
+// const a = 10     // error
+// a = 20           // error
+// const a;         // error
+
+let b = 20
+// let b = 20       // error
+b = 40              // allowed
+let k;              // allowed
+k = 70
+
+var c = 10
+var c = 20
+c = 100
+var c;
+c = 70              // all allowed
+
+var d = 10
+// const d = 7      // not allowed at runtime gives error
+console.log(d)
--- Class/27 Nov 2022/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hemlo")
-console.log("This is a dummy log.")
