75dc5f354b260294dbc704590b7e989fa7cb117b
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-18 10:43:42 +0530

    Stylised the inputs being displayed

--- Class/4- 18 Dec 2022 —/Question (18th Dec 2022)/index.js
+++ Class/4- 18 Dec 2022 —/Question (18th Dec 2022)/index.js
@@ -1,11 +1,11 @@
 const n = prompt("Enter your name:");
 const nameElement = document.getElementById("name");
-nameElement.innerHTML = n;
+nameElement.innerHTML = "<em>" + n + "</em>";
 
 const age = prompt("Enter your age:");
 const ageElement = document.getElementById("age");
-ageElement.innerHTML = age;
+ageElement.innerHTML = "<em>" + age + "</em>";
 
 const sub = prompt("Enter your favourite subject:")
 const subElement = document.getElementById("sub");
-subElement.innerHTML = sub;
+subElement.innerHTML = "<em>" + sub + "</em>";
