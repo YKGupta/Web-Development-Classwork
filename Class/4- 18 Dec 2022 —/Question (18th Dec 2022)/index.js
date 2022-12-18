a381cfc93b52dfe207c130ef149de0b0030bad55
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-18 10:34:55 +0530

    Added question on prompting and assigning values to elements via JavaScript

---
+++ Class/4- 18 Dec 2022 —/Question (18th Dec 2022)/index.js
@@ -0,0 +1,11 @@
+const n = prompt("Enter your name:");
+const nameElement = document.getElementById("name");
+nameElement.innerHTML = n;
+
+const age = prompt("Enter your age:");
+const ageElement = document.getElementById("age");
+ageElement.innerHTML = age;
+
+const sub = prompt("Enter your favourite subject:")
+const subElement = document.getElementById("sub");
+subElement.innerHTML = sub;
